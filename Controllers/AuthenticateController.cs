ab7302bd6af37294118accffce631843f4d9af6a
Robotbino <hlongwanabino@gmail.com>
2023-08-31 16:17:22 +0200

    Readme update

--- Controllers/AuthenticateController.cs
+++ Controllers/AuthenticateController.cs
@@ -116,6 +116,7 @@ namespace JWTAuthentication.Controllers
             }
 
                 return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+
             }
         }
     } 
